e6a28faa5df9d3a3b86202c5452b7cbcb91f79f1
Matze-Li <mathias.lipke@web.de>
2015-10-22 16:21:15 +0200

    Initial

---
+++ src/main/scala/HelloSpark.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+class HelloSpark {
+
+  def test(): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("HelloSpark_01"))
+
+
+
+
+  }
+}
+
+ object HelloSpark{
+    def main(args: Array[String]): Unit ={
+
+
+
+    }
+
+
+}
